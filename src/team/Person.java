ff64cf3680f71eea8924131bb023c7b4b6f08ffc
Michael <michaelgarder@outlook.com>
2014-03-01 15:21:25 +0100

    Updated Person class
    Satte attributter på, lavede getters og toString

--- src/team/Person.java
+++ src/team/Person.java
@@ -12,8 +12,42 @@ package team;
  */
 public class Person {
     private String name;
+    private int administrator, analyst, creative, finisher;
     
-    public Person(String name){
-        
+    public Person(String name, int admin, int analyst, int creative, int finisher){
+        this.name = name;
+        this.administrator = admin;
+        this.analyst = analyst;
+        this.creative = creative;
+        this.finisher = finisher;
     }
+    // Skal vi have en "fromString" constructor der kan lave en string fra en .txt fil om til en personklasse?
+    
+    @Override
+    public String toString(){
+        String str;
+        str = name + ", " + administrator + ", " + analyst + ", " + creative + ", " + finisher;
+        return str;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAdministrator() {
+        return administrator;
+    }
+
+    public int getAnalyst() {
+        return analyst;
+    }
+
+    public int getCreative() {
+        return creative;
+    }
+
+    public int getFinisher() {
+        return finisher;
+    }
+    
 }
