a5a4307d3976339ce0184cabc750e0b4c2c37b8c
Michael <michaelgarder@outlook.com>
2014-03-03 10:14:45 +0100

    Mandag formiddag
    blaaaah

--- src/team/Control.java
+++ src/team/Control.java
@@ -6,13 +6,34 @@
 
 package team;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Andreas Fisker
  */
-public class Startup {
+public class Control {
     
-    public static void main(String[] args) {
-        
+    ArrayList<Team> teamList;
+    ArrayList<Person> personList;
+    
+    public Control(){
+        teamList = new ArrayList();
+        personList = new ArrayList();
+    }
+    public void addPerson(Person p){
+        personList.add(p);
+    }
+    public void removePerson(int selected){
+        personList.remove(selected);
+    }
+    public Person getPerson(int i){
+        return personList.get(i);
+    }
+    public ArrayList<Team> getTeamList() {
+        return teamList;
+    }
+    public ArrayList<Person> getPersonList() {
+        return personList;
     }
 }
